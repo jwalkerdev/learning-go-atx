6177816e708d31380dd2125159027605d38f3445
John Walker Jr <john_walkerjr@comcast.com>
2018-10-24 12:25:33 -0500

    Add Shape interface and table driven tests

--- 002-structs-methods-interfaces/shape/shapes_test.go
+++ 002-structs-methods-interfaces/shape/shapes_test.go
@@ -3,10 +3,53 @@ package shape
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
 	want := 40.00
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	var rectangleTests = []struct {
+		shape Rectangle
+		want  float64
+		desc  string
+	}{
+		{Rectangle{12.0, 6.0}, 72.0, "Rectangles - 1"},
+		{Rectangle{6.0, 6.0}, 36.0, "Rectangles - 2"},
+		{Rectangle{0.0, 6.0}, 0.0, "Rectangles - Zero arg"},
+	}
+	for _, test := range rectangleTests {
+		t.Run(test.desc, func(t *testing.T) {
+			checkArea(t, test.shape, test.want)
+		})
+	}
+
+	var circleTests = []struct {
+		shape Circle
+		want  float64
+		desc  string
+	}{
+		{Circle{10.0}, 314.1592653589793, "Circles - 1"},
+		{Circle{100.0}, 31415.9265358979319, "Circles - 2"},
+		{Circle{0.0}, 0.0, "Circles - Zero arg"},
+	}
+	for _, test := range circleTests {
+		t.Run(test.desc, func(t *testing.T) {
+			checkArea(t, test.shape, test.want)
+		})
+	}
+
 }
