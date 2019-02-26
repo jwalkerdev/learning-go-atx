1a63245eef1b600d82c1fb178af126a149b0e096
John Walker Jr <john_walkerjr@comcast.com>
2019-02-26 11:59:06 -0600

    Updates

--- quii-go-with-tests/iteration/iteration.go
+++ quii-go-with-tests/iteration/iteration.go
@@ -2,10 +2,10 @@ package iteration
 
 const repeatCount = 5
 
-func Repeat(character string) string {
+func Repeat(characterStr string) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
-		repeated += character
+		repeated += characterStr
 	}
 	return repeated
 }
